acf7da2899eae3e7be98b5c3b663203633465f04
Jesper Gustin <sirjeppe@gmail.com>
2015-10-30 16:34:16 +0100

    * Adding ease-out timing function * Adding offset config option * Passing calculated targetYPos instead of target HTMLElement as argument to fallback function * Applying appropriate changes to README.md

--- README.md
+++ README.md
@@ -4,6 +4,38 @@ Vertical (only) animated scrolling using requestAnimationFrame.
 ## Usage
 Just include the js file and then call:
 ```javascript
-// Scroll #target into view with a duration of 300 ms
-rAFScroller.scrollIntoView(document.querySelector('#target'), 300);
+// rAFScroller.scrollIntoView() takes two arguments; an element and a config
+// object. The element should be an element in the DOM and the config object
+// can hold the following properties and values (default values shown):
+var config = {
+
+    duration: 300,
+    // A positive number representing the duration of the animation.
+
+    timingFunction: 'ease-out',
+    // Takes a string of 'ease-out' or something else. Anything else but
+    // 'ease-out' will be interpreted as 'linear' and cause a linear timing
+    // function to be applied.
+
+    offset: 0,
+    // This offset will be applied to the vertical position of the element.
+
+    fallback: function(targetYPos) {
+        throw 'No fallback method registered';
+    }
+    // Fallback function if required browser features is not available in the
+    // current browser. The only argument passed to the fallback is the
+    // calculated target Y position that the page should have scrolled to.
+
+};
+
+// Scroll #target into view with default configuration (as above)
+rAFScroller.scrollIntoView(document.querySelector('#target'));
+
+// Scroll #target into view with a fallback function using window.scrollTo()
+rAFScroller.scrollIntoView(document.querySelector('#target'), {
+    fallback: function(targetYPos) {
+        window.scrollTo(0, targetYPos);
+    }
+});
 ```
--- rAFScroller.js
+++ rAFScroller.js
@@ -2,9 +2,6 @@
 /* eslint indent: [2, 4] */
 'use strict';
 var rAFScroller = {
-    fallback: function() {
-        throw 'No fallback method registered';
-    },
     tick: function(ts) {
         var y = window.scrollY;
         var checkPos = y + rAFScroller.ppf;
@@ -22,44 +19,70 @@ var rAFScroller = {
                 // Recalc ppf (pixels per frame) every frame to ensure the set
                 // duration is kept
                 var pixelsLeftToScroll = (rAFScroller.target - y) << 0;
-                var timeLeft = rAFScroller.duration - (ts - rAFScroller.start) << 0;
+                var timeLeft = rAFScroller.config.duration - (ts - rAFScroller.start) << 0;
                 timeLeft = timeLeft <= 0 ? 1 : timeLeft;
                 delta = timeLeft === 1 ? 1 : delta;
                 rAFScroller.ppf = (pixelsLeftToScroll / timeLeft * delta) << 0;
+                if (rAFScroller.config.timingFunction === 'ease-out') {
+                    var wasNegative = false;
+                    if (rAFScroller.ppf < 0) {
+                        wasNegative = true;
+                        rAFScroller.ppf /= -1;
+                    }
+                    rAFScroller.ppf = rAFScroller.ppf * Math.log10(10 * rAFScroller.ppf) << 0;
+                    rAFScroller.ppf = wasNegative ? rAFScroller.ppf / -1 : rAFScroller.ppf;
+                }
             }
             rAFScroller.last = ts;
             scrollBy(0, rAFScroller.ppf);
             requestAnimationFrame(rAFScroller.tick);
         }
     },
-    scrollIntoView: function(elm, duration) {
+    calcTarget: function(elm, offset) {
+        var offsetTop = 0;
+        do {
+            offsetTop += elm.offsetTop;
+        } while (elm = elm.offsetParent);
+        return offsetTop + offset;
+    },
+    scrollIntoView: function(elm, config) {
         // First check for dependencies
         if ('offsetParent' in elm && 'requestAnimationFrame' in window) {
             // Make sure both elm and duration has been passed
-            if (elm && duration) {
+            if (elm) {
+                // Setup config if it's given
+                if (typeof config === 'object') {
+                    for (var i in rAFScroller.config) {
+                        if (i in config) {
+                            rAFScroller.config[i] = config[i];
+                        }
+                    }
+                }
                 // Find out how far to scroll and in what direction
                 rAFScroller.scrollDistance = elm.getBoundingClientRect().top << 0;
-                var target = elm;
-                var offsetTop = 0;
-                do {
-                    offsetTop += target.offsetTop;
-                } while (target = target.offsetParent);
-                rAFScroller.target = offsetTop;
-                rAFScroller.duration = duration;
+                rAFScroller.target = rAFScroller.calcTarget(elm, rAFScroller.config.offset);
+                rAFScroller.scrollDistance += rAFScroller.config.offset;
                 // Always start optimistically at 60 FPS
-                rAFScroller.ppf = (rAFScroller.scrollDistance / duration * (1000 / 60)) << 0;
+                rAFScroller.ppf = (rAFScroller.scrollDistance / rAFScroller.config.duration * (1000 / 60)) << 0;
                 requestAnimationFrame(rAFScroller.tick);
             }
         } else if (typeof rAFScroller.fallback === 'function') {
-            rAFScroller.fallback(elm);
+            rAFScroller.fallback(rAFScroller.target);
         } else {
             throw 'fallback was not set to a function';
+        }
+    },
+    config: {
+        timingFunction: 'ease-out',
+        duration: 300,
+        offset: 0,
+        fallback: function() {
+            throw 'No fallback method registered';
         }
     },
     last: 0,
     start: 0,
     scrollDistance: 0,
     target: 0,
-    ppf: 0,
-    duration: 0
+    ppf: 0
 };
